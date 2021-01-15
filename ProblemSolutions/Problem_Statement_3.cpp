2ad899bc170d1add0d6f187a08cd2ebe2f06b471
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2021-01-16 03:54:21 +0530

    Update Problem_Statement_3.cpp

--- ProblemSolutions/Problem_Statement_3.cpp
+++ ProblemSolutions/Problem_Statement_3.cpp
@@ -12,5 +12,7 @@ int main() {
     cin>>a>>b>>c>>d>>e;
     /*setprecision used to print long double bcz
     of including the iomainip header file*/
-    cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<d<<"\n"<<setprecision(15)<<e<<endl;
+    /*some test cases get failed so add setprecision to float to */
+    cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<setprecision(15)<<d<<"\n"<<setprecision(15)<<e<<endl;
     return 0;
+}
